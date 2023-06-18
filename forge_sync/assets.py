9c0e39c3a46c20cda65016ff33bd4cb585befbc7
André Gustavo Lucas <20599467+andrelucassc@users.noreply.github.com>
2023-06-18 12:50:37 -0300

    feat: add assets for cdi materialization

--- forge_sync/assets.py
+++ forge_sync/assets.py
@@ -1,1 +1,14 @@
 
+import os
+import requests
+
+from dagster import asset, Output, AssetMaterialization
+
+
+@asset
+def get_cdi_data(context):
+    response = requests.get(f"https://api.hgbrasil.com/finance/taxes?key={os.environ['API_KEY']}")
+    context.log.info(f"Found {response.json()}")
+
+    yield AssetMaterialization(asset_key="CDI_DATA", description="Data from HG API that has SELIC and CDI")
+    yield Output(response)
